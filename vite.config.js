7b60e907b179672fecee0dc609bc469232a2cbf9
AanantJadon <aanantjadon.dev@gmail.com>
2026-08-30 19:48:36 +0530

    Updated Website

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // GitHub Pages hosts this project inside
+  // /balaji-mobile-website/
+  base: '/balaji-mobile-website/',
 })
